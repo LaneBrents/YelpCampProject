4834f41ece278735bc886ccb5173559e0b637b28
LaneBrents <lanebrents@gmail.com>
2023-07-19 17:50:54 -0400

    new and create routes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/YelpCampProject', {
-    // useNewUrlParser: true,
+    useNewUrlParser: true,
     // useCreateIndex: true,
     // useUnifiedTopology: true
 });
@@ -22,19 +22,36 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res) => {
     res.render('home')
-})
+});
+
+//This renders the 'all campgrounds' page
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('Campgrounds/index', {campgrounds})
+});
+
+// This renders the new campground form page
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+// This sends a POST request from the new campground form page
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 })
 
+// This renders the campground info page
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
-})
+});
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
