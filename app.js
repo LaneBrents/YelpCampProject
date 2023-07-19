0de57ddc3a90419bdc86803da3ef9f2b0f24e30d
LaneBrents <lanebrents@gmail.com>
2023-07-19 16:54:57 -0400

    campground index routes

--- app.js
+++ app.js
@@ -26,10 +26,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res) => {
     res.render('home')
 })
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My Backyard', description: 'Cheap Camping!' });
-    await camp.save();
-    res.send(camp)
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('Campgrounds/index', {campgrounds})
 })
 
 app.listen(3000, () => {
