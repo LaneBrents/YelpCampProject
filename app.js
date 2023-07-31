e325793525a2807f17b7cb2144ff48510e833141
LaneBrents <lanebrents@gmail.com>
2023-07-31 13:36:18 -0400

    configuring Session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://localhost:27017/YelpCampProject', {
     useNewUrlParser: true,
     // useCreateIndex: true,
     // useUnifiedTopology: true
+    // useFindAndModify: false
 });
 
 // Logic to check if there is an error
@@ -31,6 +33,19 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig = {
+    secret: 'thisisnotagoodsecret',
+    resave: false,
+    saveUnitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7, //This will allow us to expire the session after 1 week
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
 
 
 app.use('/campgrounds', campgrounds)
