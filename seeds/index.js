7e174a49854b1468e19904b3bd7284b8dda79dd4
LaneBrents <lanebrents@gmail.com>
2023-08-12 20:19:07 -0400

    Finished map and location working

--- seeds/index.js
+++ seeds/index.js
@@ -21,15 +21,20 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++){
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            // MY USER ID
             author: '64d1236954483228bc584cf5',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quidem optio voluptates error asperiores, distinctio corrupti cupiditate eum aliquam facilis vel quas aliquid assumenda. Error, odit. Sapiente non assumenda laudantium.',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [-113.1331, 47.0202]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/dxvcu9szw/image/upload/v1691614843/YelpCamp/e0ywclfc8ixpnvoifjwo.jpg',
